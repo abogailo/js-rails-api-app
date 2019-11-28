94d0fd643a7d2b551a51bdd8d60d1adb5ec64db1
abogailo <abagailanne@gmail.com>
2019-11-28 13:01:10 -0500

    added fake gem, uncommmented cors.rb

--- educate-like-api/back-end/db/migrate/20191124195557_create_course_tags.rb
+++ educate-like-api/back-end/db/migrate/20191124195557_create_course_tags.rb
@@ -1,9 +1,9 @@
 class CreateCourseTags < ActiveRecord::Migration[6.0]
   def change
     create_table :course_tags do |t|
-      t.integer "course_id"
-      t.integer "tag_id"
-      
+      t.integer :course_id
+      t.integer :tag_id
+
       t.timestamps
     end
   end
